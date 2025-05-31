14eb91c813c1432331971f9a393a35df7f51880a
ericnoonan00 <ericnoonan00@gmail.com>
2025-05-31 17:04:04 -0400

    learned that node code doesn't work on the browser

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,10 @@
-import fs from 'fs'
+const path = '../private/json/searchresults.json'
 
-function getJSONSearchResults() {
-  const path = '../private/json/searchresults.json'
-  try {
-    if (!fs.existsSync(path))
-      throw new Error("JSON Doesn't exist")
-    const data = fs.readFileSync(path)
-    const stng = data.toString()
-    const json = JSON.parse(stng)
-    // console.log(json);
-    return json
-  } catch (err) {
-    console.error(err);
-  }
+export function getJSONSearchResults() {
+  fetch(path)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.error(err))
 }
-
-(async () => {
-  getJSONSearchResults()
-})()
